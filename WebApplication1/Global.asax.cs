a27430d6b4008d1710f94fdd0b655f4f92e27b1c
Martin Navarro <ejemplo@example.com>
2020-10-27 01:01:11 -0300

    Initial commit

---
+++ WebApplication1/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Http;
+
+namespace WebApplication1
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //Added for Angular
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
